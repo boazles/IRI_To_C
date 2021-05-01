1dd176c5b8f6bc575e7975db2ab9bc1592bf9795
boazles <boaz.leslau@gmail.com>
2021-05-01 13:49:47 -0400

    added support to running from different input files

--- wrapper.c
+++ wrapper.c
@@ -6,8 +6,16 @@
 extern void iri_sub_(int[] , int *, float *, float *, int *, int * , float *, float *, float *, float *, float[][20], float[]);
 extern void iri_web_(int[] , int *, float *, float *, int *, int * , float *,float *, int * , int * ,int *, float *, float *, float *, float[][20], float[]);
 
-int main()
+int main(int c, char *argv[])
 {
+    int index = 0;
+    if (c==2)
+        index = atoi(argv[1]);
+    else
+    {
+       printf("error: missing or too many command line arguments\n");
+        return 1;
+    }
     int line_count = 0;
     
     int jf[50]={true,true,true,false,false,
@@ -40,132 +48,142 @@ int main()
     int h_tec_max  =0; //no TEC otherwise upper boundary for integral
     int  iut=1;// for UT       =0 for LT
     int ivar =1; 
-
-    fp = fopen("params", "r");
-    if (fp == NULL)
-        exit(EXIT_FAILURE);
-
-    while ((read = getline(&line, &len, fp)) != -1) {
-        printf("Retrieved line of length %zu:\n", read);
-        //printf("%s\n", line);
-        if (line_count==0) // works only for iri_sub_ can be easily adjusted for iri_web_
-        {
-            jmag== atoi(line); //=0/1,geog/geom  coordinates could use bool
-            printf("%i\n", jmag);
-        }
-        else if (line_count==1)
-        {
-            alati= atof(line); // latitude
-            printf("%f\n", alati);
-        }
-        else if (line_count==2)
-        {
-            along = atof(line);  // longitude
-            printf("%f\n", along);
-        }
-        else if (line_count==3)
-        {
-            iyyy = atoi(line);  //year
-            printf("%i\n", iyyy);
-        }
-        else if (line_count==4)
-        {
-            mmdd = atoi(line);  // Month and  days could use day of year (Julian date)
-            printf("%i\n", mmdd);
-        }
-        else if (line_count==5) 
-        {
-            dhour =atof(line);  // Hour at UTC
-            printf("%f\n", dhour);
-        }
-        else if (line_count==6)
-        {
-            begin =atof(line); // start latitude KM
-            printf("%f\n", begin);
-        }
-        else if (line_count==7)
-        {
-            end =atof(line);  // End latitude KM
-            printf("%f\n", end);
-        }
-        else if (line_count==8)
-        {
-            step =atof(line); // Sampling step number of steps cannot exceed 1000
-            printf("%f\n", step);
-        }
-        line_count++;
-        printf("%d\n",line_count);
-    }
-    fclose(fp);
-    int month = floor(mmdd%100); // formatting graph
-    int day = mmdd%100;
-    char date[100];
-    int minutes = floor(fmodf(dhour, floor(dhour))*60);
-    char min[3];
-    if (minutes >=10)
-        sprintf(min,"%d",  minutes);
-    else
-        sprintf(min, "%d%d", 0, minutes);
-    sprintf(date, "%d %d %d %d:%s %s\n", (int)month, day ,iyyy, (int)dhour, min, "UTC" );
-    printf("%s\n", date);
-    printf("%i, %f, %f,%i, %i, %f, %f, %f, %f\n", jmag, alati, along, iyyy,mmdd,dhour, begin, end, step);
-    //iri_web_(jf, &jmag, &alati, &along, &iyyy, &mmdd , &dhour,&height , &h_tec_max,&iut, &ivar, &begin , &end, &step,  out, oarr);
-    iri_sub_(jf, &jmag, &alati, &along, &iyyy, &mmdd , &dhour, &begin , &end, &step,  out, oarr);
-    printf("computed iri model\n");
-    /* File pointer to hold reference to our file */
-    FILE * fPtr;
-
-
-    /* 
-     * Open file in w (write) mode. 
-     * "data/file1.txt" is complete path to create file
-     */
-    fPtr = fopen("output.dat", "w");
-    printf("saving the iri model\n");
-
-    /* fopen() return NULL if last operation was unsuccessful */
-    if(fPtr == NULL)
+    char file_name[10] ;
+    sprintf(file_name, "%s%d", "params", index);
+    fp = fopen(file_name, "r");
+    if (fp != NULL)
     {
-        /* File not created hence exit */
-        printf("Unable to create file.\n");
-        exit(EXIT_FAILURE);
-    }
 
-   
-    float fact = 1.6*10E-19/sqrt(8.85*10E-12*9.1*10E-31);
-    for (int i = 0 ; i<1000 ;i++)
-    {
-       
-        for (int j =0 ; j<1 ; j++)
+        while ((read = getline(&line, &len, fp)) != -1) 
         {
-            if (out[i][j]!=-1.0)
-            {
-                fprintf(fPtr,"%f ",fact*sqrt(out[i][j])/(2*M_PI*1000000));
-                fprintf(fPtr,"%f",begin+i*step);
-                 fprintf(fPtr,"\n");
+                printf("Retrieved line of length %zu:\n", read);
+                //printf("%s\n", line);
+                if (line_count==0) // works only for iri_sub_ can be easily adjusted for iri_web_
+                {
+                    jmag== atoi(line); //=0/1,geog/geom  coordinates could use bool
+                    printf("%i\n", jmag);
+                }
+                else if (line_count==1)
+                {
+                    alati= atof(line); // latitude
+                    printf("%f\n", alati);
+                }
+                else if (line_count==2)
+                {
+                    along = atof(line);  // longitude
+                    printf("%f\n", along);
+                }
+                else if (line_count==3)
+                {
+                    iyyy = atoi(line);  //year
+                    printf("%i\n", iyyy);
+                }
+                else if (line_count==4)
+                {
+                    mmdd = atoi(line);  // Month and  days could use day of year (Julian date)
+                    printf("%i\n", mmdd);
+                }
+                else if (line_count==5) 
+                {
+                    dhour =atof(line);  // Hour at UTC
+                    printf("%f\n", dhour);
+                }
+                else if (line_count==6)
+                {
+                    begin =atof(line); // start latitude KM
+                    printf("%f\n", begin);
+                }
+                else if (line_count==7)
+                {
+                    end =atof(line);  // End latitude KM
+                    printf("%f\n", end);
+                }
+                else if (line_count==8)
+                {
+                    step =atof(line); // Sampling step number of steps cannot exceed 1000
+                    printf("%f\n", step);
+                }
+                line_count++;
+                printf("%d\n",line_count);
             }
-        }
-    }
-    
+            fclose(fp);
+            int month = floor(mmdd%100); // formatting graph
+            int day = mmdd%100;
+            char date[100];
+            int minutes = floor(fmodf(dhour, floor(dhour))*60);
+            char min[3];
+            if (minutes >=10)
+                sprintf(min,"%d",  minutes);
+            else
+                sprintf(min, "%d%d", 0, minutes);
+            sprintf(date, "%d %d %d %d:%s %s\n", (int)month, day ,iyyy, (int)dhour, min, "UTC" );
+            printf("%s\n", date);
+            printf("%i, %f, %f,%i, %i, %f, %f, %f, %f\n", jmag, alati, along, iyyy,mmdd,dhour, begin, end, step);
+            //iri_web_(jf, &jmag, &alati, &along, &iyyy, &mmdd , &dhour,&height , &h_tec_max,&iut, &ivar, &begin , &end, &step,  out, oarr);
+            iri_sub_(jf, &jmag, &alati, &along, &iyyy, &mmdd , &dhour, &begin , &end, &step,  out, oarr);
+            printf("computed iri model\n");
+            /* File pointer to hold reference to our file */
+            FILE * fPtr;
 
-    /* Close file to save file data */
-    fclose(fPtr);
+
+            /* 
+            * Open file in w (write) mode. 
+            * "data/file1.txt" is complete path to create file
+            */
+            char outfile[20];
+            sprintf(outfile, "%s%d%s", "output", index , ".dat");
+            fPtr = fopen(outfile, "w");
+            printf("saving the iri model to %s\n", outfile);
+
+            /* fopen() return NULL if last operation was unsuccessful */
+            if(fPtr == NULL)
+            {
+                /* File not created hence exit */
+                printf("Unable to create file.\n");
+                exit(EXIT_FAILURE);
+            }
+
+        
+            float fact = 1.6*10E-19/sqrt(8.85*10E-12*9.1*10E-31); // change from density to frequency using the oscilator formula wp = sqrt(n*e^2/(eo*me)) using permittivity of free space
+            for (int i = 0 ; i<1000 ;i++)
+            {
+            
+                for (int j =0 ; j<1 ; j++)
+                {
+                    if (out[i][j]!=-1.0)
+                    {
+                        fprintf(fPtr,"%f ",fact*sqrt(out[i][j])/(2*M_PI*1000000));
+                        fprintf(fPtr,"%f",begin+i*step);
+                        fprintf(fPtr,"\n");
+                    }
+                }
+            }
+            
+
+            /* Close file to save file data */
+            fclose(fPtr);
 
 
-    /* Success message */
-    printf("File created and saved successfully. :) \n");
-    int NUM_COMMANDS = 4;
-    char title[400];
-    char st[20]= "EDP for";
-    char beg[20] = "set title \"";
-    char utc[10] = "\"";
-    sprintf (title , "%s %s %s  ",beg, st, date);
-    printf( "%s\n", title);
-    char * commandsForGnuplot[] = {title, "set xlabel \"Plasma Frequency (MHz)\"", "set ylabel \"Altitude (Km)\"","plot 'output.dat'"};
-    FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
-    for (int i=0; i < NUM_COMMANDS; i++)
-    {
-        fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
-    }
+            /* Success message */
+            printf("File created and saved successfully. :) \n");
+            int NUM_COMMANDS = 4;
+            char title[400];
+            char st[20]= " EDP for";
+            char beg[20] = "set title \"";
+            char utc[10] = "\'";
+            sprintf (title , "%s%s %s  ",beg, st, date);
+            printf( "%s\n", title);
+            char fl[50];
+            char pl[20] = "plot \'";
+            sprintf (fl , "%s%s%s"  , pl, outfile, utc);
+            printf("%s\n", title);
+            printf("%s\n", fl);
+            char * commandsForGnuplot[] = {title, "set xlabel \"Plasma Frequency (MHz)\"", "set ylabel \"Altitude (Km)\"",fl};
+            FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
+            for (int i=0; i < NUM_COMMANDS; i++)
+            {
+                fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
+            }
+       }
    return 0;
 }
